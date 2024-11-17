f9cc9d5f2be8801132083dbaf7bc3258ec10b879
Victor Sun <victor@Victors-MacBook-Air.local>
2024-11-17 11:31:55 -0500

    v2, now a slideshow of 3 gifs

--- gui.py
+++ gui.py
@@ -1,137 +1,119 @@
-import time
-import threading
 import customtkinter as ctk
+from PIL import Image, ImageTk
 
-from customtkinter import filedialog
-
-from PIL import Image
-
- 
-
-ctk.set_appearance_mode("Light")        
- 
-
+root = ctk.CTk()
+ctk.set_appearance_mode("Light")
 ctk.set_default_color_theme("green")
 
-
-#Customize
 mainFont = ("Montserrat", 24)
+windowText = "Galaxy X, Y, and Z"
+root.title(windowText)
+root.geometry("1000x1000")
 
-windowText = "placeholder"
+def load_gif(path):
+    gif = Image.open(path)
+    frames = []
+    for _ in range(gif.n_frames):
+        frames.append(ImageTk.PhotoImage(gif.copy().convert("RGBA")))
+        gif.seek(_)  
+    return frames
 
-mainTitleText = "title"
+frames1 = load_gif("/Users/victor/Downloads/fwgifforgalaxy/fixed_x_animation.gif")
+frames2 = load_gif("/Users/victor/Downloads/fwgifforgalaxy/fixed_y_animation.gif")
+frames3 = load_gif("/Users/victor/Downloads/fwgifforgalaxy/fixed_z_animation.gif")
 
- 
+pic1 = ctk.CTkLabel(root, text="", font=mainFont)
+pic1.place(relx=0.5, rely=0.5, anchor="center")
 
-class program(ctk.CTk):
-    
-    def __init__(self, *args, **kwargs):
- 
-        
-        super().__init__(*args, **kwargs)
+pic2 = ctk.CTkLabel(root, text="", font=mainFont)
+pic2.place(relx=1.5, rely=0.5, anchor="center")
 
-        self.title(windowText)
+pic3 = ctk.CTkLabel(root, text="", font=mainFont)
+pic3.place(relx=2.5, rely=0.5, anchor="center")
 
-        self.geometry("1400x1000")
+x = 0.5
+stage = 1
+frame_index1 = 0
+frame_index2 = 0
+frame_index3 = 0
 
-        self.label = ctk.CTkLabel(self, text=mainTitleText, font=mainFont)
-        self.label.place(relx=0.5, rely=0.05, anchor="center")
+def update_gif_labels():
+    global frame_index1, frame_index2, frame_index3
+    pic1.configure(image=frames1[frame_index1])
+    pic2.configure(image=frames2[frame_index2])
+    pic3.configure(image=frames3[frame_index3])
+    frame_index1 = (frame_index1 + 1) % len(frames1)
+    frame_index2 = (frame_index2 + 1) % len(frames2)
+    frame_index3 = (frame_index3 + 1) % len(frames3)
+    root.after(100, update_gif_labels)
 
-        self.ready = ctk.CTkLabel(self, text="", font=mainFont)
-        self.ready.place(relx=0.5, rely=0.8, anchor="center")
+def slide():
+    global frame_index1, frame_index2, frame_index3
 
+    global x, stage
+    print(x)
 
-
-
-
-        self.pic1 = ctk.CTkLabel(self, text="", font=mainFont)
-        self.pic1.place(relx=0.2, rely=0.7, anchor="center")
-
-        self.pic2 = ctk.CTkLabel(self, text="", font=mainFont)
-        self.pic2.place(relx=0.8, rely=0.7, anchor="center")
-
-        
-
-
-        def generate():
-            if self.ready.cget('text') == "Ready":
-
-                result = ctk.CTkToplevel(self)
-
-                result.title("Result")
-
-                result.test = ctk.CTkLabel(result, text="a", font=mainFont)
-                result.test.place(relx=0.5, rely=0.5, anchor="center")
-        
-
-        self.button = ctk.CTkButton(self, text="Generate Result", font=mainFont, command=lambda : generate())
-        self.button.place(relx=0.5, rely=0.9, anchor="center")
-
-        
-
-
-
-
-
-        self.selectText1 = ctk.CTkLabel(self, text="No file selected", font=mainFont)
-        self.selectText1.place(relx=0.2, rely=0.3, anchor="w")
-
-        self.selectText2 = ctk.CTkLabel(self, text="No file selected", font=mainFont)
-        self.selectText2.place(relx=0.2, rely=0.4, anchor="w")
-
-
-
-        self.selectButton1 = ctk.CTkButton(self, text="Select file", font=mainFont, command=lambda : select_file(self.selectText1))
-        self.selectButton1.place(relx=0.1, rely=0.3, anchor="center")
-
-        self.selectButton2 = ctk.CTkButton(self, text="Select file", font=mainFont, command=lambda : select_file(self.selectText2))
-        self.selectButton2.place(relx=0.1, rely=0.4, anchor="center")
-
-
-        def select_file(thing):
-
-            filetypes = [("Pictures", ".png .jpg .jpeg")
-                         ]
+    if stage == 1:
+        if x >= -0.49:
+            x -= 0.05
+            pic1.place(relx=x)
+            pic2.place(relx=x + 1)
+            pic3.place(relx=x + 2)
+            root.after(20, slide)
+        else:
+            stage = 2
             
-            file = filedialog.askopenfilename(filetypes=filetypes)
-        
+    elif stage == 2:
+        if x >= -1.49:
+            x -= 0.05
+            pic1.place(relx=x)
+            pic2.place(relx=x + 1)
+            pic3.place(relx=x + 2)
+            root.after(20, slide)
+        else:
+            stage = 3
 
-            if file != "":
+    frame_index1 = 0
+    frame_index2 = 0
+    frame_index3 = 0
 
-                thing.configure(text=file)
+def slideRight():
+    global x, stage
+    global frame_index1, frame_index2, frame_index3
 
-                if thing == self.selectText1:
-                    img = ctk.CTkImage(light_image=Image.open(file), size=(400,400))
+    print(x)
+    if stage == 3:
+        if x <= -0.5:
+            x += 0.05
+            pic1.place(relx=x)
+            pic2.place(relx=x + 1)
+            pic3.place(relx=x + 2)
+            root.after(20, slideRight)
+        else:
+            stage = 2
+    elif stage == 2:
+        if x <= 0.5:
+            x += 0.05
+            pic1.place(relx=x)
+            pic2.place(relx=x + 1)
+            pic3.place(relx=x + 2)
+            root.after(20, slideRight)
+        else:
+            stage = 1
+
+    frame_index1 = 0
+    frame_index2 = 0
+    frame_index3 = 0
+
+update_gif_labels()
+
+back = ctk.CTkButton(root, text="<", font=mainFont, command=slide)
+forward = ctk.CTkButton(root, text=">", font=mainFont, command=slideRight)
+
+back.place(relx=0.4, rely=0.9, anchor="center")
+forward.place(relx=0.6, rely=0.9, anchor="center")
 
 
-                    self.pic1.configure(image=img)
-                else:
-                    img = ctk.CTkImage(light_image=Image.open(file), size=(400,400))
-
-
-                    self.pic2.configure(image=img)
                 
-            else:
-                thing.configure(text="No file selected")
 
-
-
-
-            if self.selectText1.cget('text') != "No file selected" and self.selectText2.cget('text') != "No file selected":
-    
-
-                self.ready.configure(text="Ready")
-
-            else:
-                
-                self.ready.configure(text="")
-
-
-
-
- 
- 
-if __name__ == "__main__":
-
-    
-    program().mainloop()  
+root.mainloop()
